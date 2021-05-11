74cb68493ea017c9f53ee597c808d693c75bf7da
Harrison Liew <harrisonliew@gmail.com>
2021-05-11 11:23:32 -0700

    fix issue with tieoffs when having more than 1 corner of a certain type

--- synthesis/genus/__init__.py
+++ synthesis/genus/__init__.py
@@ -19,6 +19,7 @@ from specialcells import CellType, SpecialCell
 
 import os
 import json
+from collections import Counter
 
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../common"))
@@ -291,7 +292,16 @@ class Genus(HammerSynthesisTool, CadenceTool):
         # Limit "no delay description exists" warnings
         self.verbose_append("set_db message:WSDF-201 .max_print 20")
         self.verbose_append("set_db use_tiehilo_for_const duplicate")
-        self.verbose_append("add_tieoffs -high {HI_TIEOFF} -low {LO_TIEOFF} -max_fanout 1 -verbose".format(HI_TIEOFF=tie_hi_cell, LO_TIEOFF=tie_lo_cell))
+
+        # If there is more than 1 corner or a certain type, use lib cells for only the active analysis view
+        corner_counts = Counter(list(map(lambda c: c.type, self.get_mmmc_corners())))
+        if any(cnt>1 for cnt in corner_counts.values()):
+            self.verbose_append("set ACTIVE_VIEW [string map {.setup_view "" .hold_view "" .extra_view ""} [get_db analysis_view:[get_analysis_views] .name]]")
+            self.verbose_append("set HI_TIEOFF [get_db base_cell:{TIE_HI_CELL} .lib_cells -if {{ .library.default_opcond == $ACTIVE_VIEW }}]".format(TIE_HI_CELL=tie_hi_cell))
+            self.verbose_append("set LO_TIEOFF [get_db base_cell:{TIE_LO_CELL} .lib_cells -if {{ .library.default_opcond == $ACTIVE_VIEW }}]".format(TIE_LO_CELL=tie_lo_cell))
+            self.verbose_append("add_tieoffs -high $HI_TIEOFF -low $LO_TIEOFF -max_fanout 1 -verbose")
+        else:
+            self.verbose_append("add_tieoffs -high {HI_TIEOFF} -low {LO_TIEOFF} -max_fanout 1 -verbose".format(HI_TIEOFF=tie_hi_cell, LO_TIEOFF=tie_lo_cell))
         return True
 
     def generate_reports(self) -> bool:
